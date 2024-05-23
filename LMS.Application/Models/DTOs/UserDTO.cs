d132a9173dc4bc5026f1faf2845c90acd763f602
Rami <ramiakash84@gmail.com>
2024-05-23 13:17:08 +0300

    added role to userDto

--- LMS.Application/Models/DTOs/UserDTO.cs
+++ LMS.Application/Models/DTOs/UserDTO.cs
@@ -2,25 +2,30 @@ using LMS.Domain.Entities;
 
 namespace LMS.Application.Models.DTOs
 {
-    public class UserDTO
-    {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailId { get; set; }
+	public class UserDTO
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailId { get; set; }
 		public string Password { get; set; }
 		public int Status { get; set; }
-        public string StatusText { get; set; }
+		public string StatusText { get; set; }
+		public int Role { get; set; }
+		public string RoleText { get; set; }
 		public UserDTO() { }
 
 		public UserDTO(User user)
-        {
-            Id = user.Id;
-            FirstName = user.FirstName;
-            LastName = user.LastName;
-            EmailId = user.EmailId;
-            Status = (int)user.Status;
-            StatusText = user.Status.ToString();
-        }
-    }
+		{
+			Id = user.Id;
+			FirstName = user.FirstName;
+			LastName = user.LastName;
+			EmailId = user.EmailId;
+			Status = (int)user.Status;
+			StatusText = user.Status.ToString();
+			Role = (int)user.Role;
+			RoleText = user.Role.ToString();
+
+		}
+	}
 }
