e7b13b69743a827985a4af7c5e10130688d5cf93
Rami <ramiakash84@gmail.com>
2024-05-26 12:57:50 +0300

    migrations

---
+++ LMS.Infrastructure/Migrations/20240526093912_emailnamechange.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class emailnamechange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmailId",
+                table: "Users",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "EmailId");
+        }
+    }
+}
